91933421eb2f0ff8f272cbb09573116f916b93f1
Victor <victor.movileanu@check24.de>
2022-11-14 12:59:08 +0100

    set up mysql as database

--- src/project/settings.py
+++ src/project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "db_mysql",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "0.0.0.0",
+        "PORT": 3306,
     }
 }
 
